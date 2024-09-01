a216b839f67c3f684b12ad7b025ca3eb5d72d8a6
ishitak007@outlook.com <ishitak007@outlook.com>
2024-09-01 11:25:45 +0530

    files

---
+++ shut_down.py
@@ -0,0 +1,13 @@
+"""
+Author: Ishita Katyal
+Date created: 06/25/2023
+Date last updated: 06/25/2023
+Topic: Automated Shut-down
+Description:
+    1. Click on the executable to automatically shut down the system.
+"""
+
+import os
+
+# shutting the system down here
+os.system("shutdown /s /t 1")
